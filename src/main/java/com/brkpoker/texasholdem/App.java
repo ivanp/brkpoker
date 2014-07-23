18057aee1eaf4af9f41c64e8c4cb55a22ce7faaa
Ivan <ivan@primaguna.com>
2014-07-23 10:01:48 +0800

    Imported texasholdem and netty-socketio

--- src/main/java/com/brkpoker/texasholdem/App.java
+++ src/main/java/com/brkpoker/texasholdem/App.java
@@ -1,13 +1,28 @@
 package com.brkpoker.texasholdem;
 
+/* Socket IO */
+import com.corundumstudio.socketio.listener.*;
+import com.corundumstudio.socketio.*;
+
+/* Texas Holdem */
+import org.ozsoft.texasholdem.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	public static final int ServerPort = 8080;
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(ServerPort);
+        System.out.println( "Server configured at port " +  ServerPort);
+		
+		Table table = new Table(TableType.NO_LIMIT, 20);
+		System.out.println( "Created table" );
     }
 }
